9e700db9d2c0817248895deaedc8109669367aef
Guillermo Cacho <guillermo.cacho.v@gmail.com>
2023-02-18 11:15:30 -0600

    Maquetacion de HTML con JS

--- js-CursoPractico/main.js
+++ js-CursoPractico/main.js
@@ -4,19 +4,110 @@ const menuHambIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const shoppingCartLogo = document.querySelector('.navbar-shopping-cart');
 const shoppingCartMenu = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHambIcon.addEventListener('click', toggleMobileMenu);
 shoppingCartLogo.addEventListener('click', toggleShoppingCart);
 
 function toggleDesktopMenu(){
+    const isShoppingCartMenuClosed = shoppingCartMenu.classList.contains('inactive');
+       
+    if (!isShoppingCartMenuClosed){
+        shoppingCartMenu.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
+    const isShoppingCartMenuClosed = shoppingCartMenu.classList.contains('inactive');
+       
+    if (!isShoppingCartMenuClosed){
+        shoppingCartMenu.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
 }
 
 function toggleShoppingCart(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+       
+    if (!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
     shoppingCartMenu.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push({
+    name:'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name:'Pantalla',
+    price: 344,
+    image: 'https://www.lg.com/mx/images/televisores/md06124536/gallery/70UM73_DZ01.jpg'
+});
+productList.push({
+    name:'Laptop',
+    price: 500,
+    image: 'https://consumer.huawei.com/content/dam/huawei-cbg-site/south-pacific/ph/mkt/plp/laptops/img/img-0914/kv.jpg'
+});
+
+/* Inicio Maquetando HTML Con JS */
+
+/* <div class="product-card">
+    <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+    <div class="product-info">
+    <div>
+        <p>$120,00</p>
+        <p>Bike</p>
+    </div>
+    <figure>
+        <img src="./icons/bt_add_to_cart.svg" alt="">
+    </figure>
+    </div>
+</div> */
+
+function renderProducts(arr){
+    for (product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+    
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCard = document.createElement('img');
+        productImgCard.setAttribute('src', './icons/bt_add_to_cart.svg')
+    
+        productInfoFigure.appendChild(productImgCard);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
+
+/* Fin Maquetando HTML Con JS */
